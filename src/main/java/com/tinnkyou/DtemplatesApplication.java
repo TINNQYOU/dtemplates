4a7542571054b0d2782c9f87c8e0d018fb28dca7
wb-cq355812 <wb-cq355812@alibaba-inc.com>
2019-02-13 18:21:02 +0800

    初始化

---
+++ src/main/java/com/tinnkyou/DtemplatesApplication.java
@@ -0,0 +1,15 @@
+package com.tinnkyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wb-cq355812
+ * @create 2019/02/13
+ */
+@SpringBootApplication
+public class DtemplatesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DtemplatesApplication.class);
+    }
+}
---
+++ src/main/java/com/tinnkyou/restfulApi/RestfulApiController.java
@@ -0,0 +1,13 @@
+package com.tinnkyou.restfulApi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author wb-cq355812
+ * @create 2019/02/13
+ */
+@Controller
+@RequestMapping(value = "/restful")
+public class RestfulApiController {
+}
