9d98e1fb9ed34bc97f4d722aa29764d144e644a2
TinnKyou <285278827@qq.com>
2019-06-19 22:00:42 +0800

    测试

--- src/main/java/com/tinnkyou/config/TestController.java
+++ src/main/java/com/tinnkyou/config/TestController.java
@@ -4,15 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * @author wb-cq355812
- * @create 2019/06/19
- */
+
 @Controller
 public class TestController {
     @RequestMapping(value = "/test",method = RequestMethod.GET)
     @ApiAction(name = "/test",method = RequestMethod.GET,params = {"name","age"})
     public String testContro(String name,String age){
+        System.out.println(name + " " + age);
         return "";
     }
 }
